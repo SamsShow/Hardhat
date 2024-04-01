0ee8531eebc1a1d914e3678a135186aceb8cc99f
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2024-04-01 21:04:01 +0530

    ✨ Etherscan & Interaction

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,6 +13,18 @@ async function main() {
   //   await simpleStorage.deployed(); -> This is not needed in the higher versions of ethers.js
 
   console.log("MyContract deployed to:", simpleStorage.target);
+  console.log(hre.network.config);
+
+//   await verify(simpleStorage.target, []);
+
+  const currentvalue = await simpleStorage.retrieve();
+  console.log("Current value of the contract is:", currentvalue);
+
+  //Update Current value
+  const transaction = await simpleStorage.store(100);
+  await transaction.wait(1);
+  const updatedvalue = await simpleStorage.retrieve();
+  console.log(updatedvalue);
 }
 
 main()
